488066fc90593c244eb40835d7470ab7d8c9941b
DmJSCourse <dimonua@gmail.com>
2020-09-01 21:21:33 +0300

    Updates

---
+++ precache-manifest.2ba8552937a16c2cbdc2e0516d7b7926.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "142f18ca74ad2266bcba1a7975b5b25d",
+    "url": "/react_movies-list/index.html"
+  },
+  {
+    "revision": "5131f936d521ae25616a",
+    "url": "/react_movies-list/static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "df8c0df09cfe4c2c85b8",
+    "url": "/react_movies-list/static/css/main.8a2ebc01.chunk.css"
+  },
+  {
+    "revision": "5131f936d521ae25616a",
+    "url": "/react_movies-list/static/js/2.a151ed51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_movies-list/static/js/2.a151ed51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df8c0df09cfe4c2c85b8",
+    "url": "/react_movies-list/static/js/main.bc688e4c.chunk.js"
+  },
+  {
+    "revision": "33e1950d215f146e2d9d",
+    "url": "/react_movies-list/static/js/runtime-main.08f1ce8c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "/react_movies-list/precache-manifest.2ba8552937a16c2cbdc2e0516d7b7926.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_movies-list/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
